e09012219a47dc45753631d5cfd345d7af952a18
巧椰 菊地 <220065@jc-21.jp>
2024-01-15 15:09:11 +0900

    Astarアルゴリズムを作り始めたナビAIで

--- AI/NavigationAI.cpp
+++ AI/NavigationAI.cpp
@@ -1,7 +1,69 @@
 #include "NavigationAI.h"
 
+NavigationAI::NavigationAI()
+{
+}
+
+NavigationAI::~NavigationAI()
+{
+}
+
 void NavigationAI::Initialize()
 {
+	
+	//これがstageの大きさになる
+	h_ = 30;
+	w_ = 30;
+
+
+	start_ = std::make_pair(-1, -1);//スタート地点から探索を始める
+	target_ = std::make_pair(-1, -1);
+
+	for (int i = 0; i < h_; i++) {
+		map_.emplace_back(w_);//w分の行を先にh列分だけ確保しておく
+		dist_.emplace_back(w_);
+		rest_.emplace_back(w_);
+	}
+
+	//壁は-1でいいよね?
+	for (int f = 0; f < w_; f++) {
+		int n = 0;
+		cin >> n;
+		map_.at(i).at(f) = n;
+		rest_[i][f] = Pair(i, f); //restにxy座標を入れる
+	}
+
+
+	if (map_.at(y).at(x) == -1) {
+		cout << "まちがえてますよ" << endl;
+		return;
+	}
+
+	start_ = std::make_pair(y, x);//スタート地点から探索を始める
+	rest_.at(0).at(0) = Pair(y, x); //スタート地点の座標
+	que_.emplace(0, Pair(y, x));//スタート地点から探索を始める
+
+	cout << "ゴール入力" << endl;
+	int goalX, goalY;
+	cout << "X = "; cin >> goalX;
+	cout << "Y = "; cin >> goalY;
+	goalX -= 1;
+	goalY -= 1;
+
+	if (map_.at(goalY).at(goalX) == -1) {
+		cout << "まちがえてますよ" << endl;
+		return;
+	}
+
+	target_ = std::make_pair(goalY, goalX);
+
+	const int Inf = 9999999;//まだ探索していない
+	dist_.assign(h_, vector<long long>(w_, Inf));//初期化
+
+	dist_.at(y).at(x) = map_.at(y).at(x); //スタート地点のコストを入れる
+
+
+
 }
 
 void NavigationAI::Release()
@@ -10,4 +72,80 @@ void NavigationAI::Release()
 
 void NavigationAI::Astar()
 {
+
+	while (!que_.empty())
+	{
+		PP now = que_.top();//今いる場所を確保
+		que_.pop();
+
+		for (int i = 0; i < 4; i++) {
+
+			int ny = now.second.first;//今いる場所 NowY
+			int nx = now.second.second;
+			int sy = ny + moveY[i];//今から探索する場所 SecondY
+			int sx = nx + moveX[i];
+
+			// 画面外なら
+			if (sy < 0 || sy >= h_ || sx < 0 || sx >= w_) {
+				continue;
+			}
+
+			//壁なら
+			if (map_.at(sy).at(sx) == -1) {
+				continue;
+			}
+
+			//これから探索するところが今いる位置から行くとそこまでの最短距離（dist＋vのコスト分で今現在わかっている最短距離）でないなら。
+			if (dist_.at(sy).at(sx) <= dist_.at(ny).at(nx) + map_.at(sy).at(sx)) {
+				continue; //今から探索しようとしてる場所はもし一度も行ってなかったらINFが入ってて絶対更新される
+			}
+
+			rest_.at(sy).at(sx) = Pair(ny, nx); //最短経路が出た探索済みの座標に探索前どこにいたかの情報を入れて後で経路復元に使う
+			dist_.at(sy).at(sx) = dist_.at(ny).at(nx) + map_.at(sy).at(sx) + Heuristic(sy, sx);//ヒューリスティック分も込みで最短距離の更新
+
+			que_.emplace(PP(dist_.at(sy).at(sx), Pair(sy, sx)));//次の探索候補を入れておく
+		}
+	}
+
+}
+
+void NavigationAI::Path_Search()
+{
+
+	//ゴール地点
+	int i = target_.first;
+	int f = target_.second;
+
+	cout << dist_.at(i).at(f) << endl;//パイザは右下がゴールのためゴールの位置までの最短コストを表示
+
+	ans_.emplace(i, f);//ゴール地点だけあらかじめ入れとく
+
+	while (true) {
+
+		for (int n = 0; n < 4; n++) {
+			int y = i;
+			int x = f;
+			y += moveY[n]; //上下探索
+			x += moveX[n];
+			if (y < 0 || y >= h_ || x < 0 || x >= w_) {// 画面外なら
+				continue;
+			}
+			if (rest_.at(i).at(f) != Pair(y, x)) {//上下探索する時の座標とrestに入ってるその場所に行く前に居た座標と照らし合わせてその値が同じならansに入れる
+				continue;
+			}
+			ans_.emplace(rest_.at(i).at(f)); //通ってきた座標を入れる
+			i = y;  //次はその前にいた座標の上下左右を探索するため更新
+			f = x;  //restの値を入れようとするとiとfで探索するため値が同時に更新されてしまい曲がり角でおかしくなってしまう
+		}
+
+		if (i == start_.first && f == start_.second) {//初期位置に戻ったら
+			break;
+		}
+	}
+
+}
+
+int NavigationAI::Heuristic(int _x, int _y)
+{
+	return 0;
 }
--- AI/NavigationAI.h
+++ AI/NavigationAI.h
@@ -1,12 +1,34 @@
 #pragma once
 #include "AI.h"
 #include "../Engine/GameObject.h"
+#include <vector>
+#include <functional>
+#include <queue>
+#include <stack>
+
+//const int moveY[8] = { 0, 0, 1,-1, 1 ,1,-1,-1 };//上下左右に移動（探索）するための配列。二つまとめて縦に見ると上下左右
+//const int moveX[8] = { 1,-1, 0, 0, 1,-1 ,1,-1 };
+
+const int moveY[4] = { 0,0,1,-1 };//上下左右に移動（探索）するための配列。二つまとめて縦に見ると上下左右
+const int moveX[4] = { 1,-1,0,0 };
+
+using std::vector;
+using std::pair;
+using Pair = pair<long long, long long>; // { distance, from }
+using Graph = vector<vector<long long>>; //二次元配列上のグラフ
+using PP = pair<long long, pair<int, int>>;//firstにコスト。secondにそのコストの位置と同じ座標を入れる
+using PqPP = std::priority_queue<PP, vector<PP>, std::greater<PP>>; //昇順で要素を入れておく.昇順って小さい順って意味だからな
 
 /// <summary>
 /// キャラクターAIとメタAIに情報を提供する
 /// </summary>
 class NavigationAI : AI
 {
+	//コンストラクタ
+	NavigationAI();
+
+	//デストラクタ
+	~NavigationAI();
 
 	///////////オーバーライドした関数/////////
 	
@@ -20,9 +42,48 @@ class NavigationAI : AI
 
 	void Astar();
 
+	void Path_Search();
+
+	int Heuristic(int _x, int _y);
+
+
+	//////////////アクセス関数//////////////
+	
+	/// <summary>
+	/// 探索を始める位置
+	/// </summary>
+	/// <param name="x">始める位置のx座標</param>
+	/// <param name="y">始める位置のy座標</param>
+	void SetStartPos(float x, float y) { start_ = std::make_pair(x, y); }
+
+	/// <summary>
+	/// 目標とする位置
+	/// </summary>
+	/// <param name="x">目標の位置のx座標</param>
+	/// <param name="y">目標の位置のy座標</param>
+	void SetTargetPos(float x, float y){ target_ = std::make_pair(x, y); }
+	
+	
 
 
 private:
+
+	int h_, w_; //縦幅と横幅
+	
+	//スタートとゴールはfloatにしたほうがいい？検討中
+	pair<int, int> start_;
+	pair<int, int> target_;
+
+	Graph map_;    //マップのコストを入れる。
+	Graph dist_; //マップの位置に連動してその頂点までどのぐらいの歩数で行けるか追加する
+	vector<vector<Pair>> rest_; //経路復元に使用するため、この中には一個前にいたxy座標を入れておく
+
+	PqPP que_; //探索済みの場所を記憶しておく。一度行った場所だけを座標で横並びで覚えておけばいい
+	//newメモ ヒューリスティックで探した合計が一番近いやつってこれでいいんじゃね
+	//その位置までのヒューリスティックとコストを合わせたりなんなりするところ
+
+	std::stack <Pair> ans_;
+
 	
 };
 
