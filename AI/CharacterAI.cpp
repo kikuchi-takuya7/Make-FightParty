17cc0ccbc3365fc9ec115eadd49760e1183abfa8
巧椰 菊地 <220065@jc-21.jp>
2024-01-19 11:00:24 +0900

    AIの処理をコメントアウトして例外が出ないようにした

--- AI/CharacterAI.cpp
+++ AI/CharacterAI.cpp
@@ -29,7 +29,7 @@ void CharacterAI::MoveEnemy()
 {
 
 	//•‚“®¬”“_•ª‚à‚µ‚Á‚©‚èl‚¦‚Ä‚¨‚±‚¤
-	pNavigationAI_->SetEnemyPos(pEnemy_->GetPosition());
-	pEnemy_->SetPosition(Float3Add(pEnemy_->GetPosition(), pNavigationAI_->Astar()));
+	//pNavigationAI_->SetEnemyPos(pEnemy_->GetPosition());
+	//pEnemy_->SetPosition(Float3Add(pEnemy_->GetPosition(), pNavigationAI_->Astar()));
 
 }
