d73975accce45a3f2dc426bf00f88459fa28b2b5
巧椰 菊地 <220065@jc-21.jp>
2024-02-21 11:29:20 +0900

    モデルを作って、ボーンありのモデルを完成させた。アニメーションはまだ

--- Character/Player/Player.cpp
+++ Character/Player/Player.cpp
@@ -43,7 +43,7 @@ void Player::ChildInitialize()
 	status_.attackPower = PLAYER_ATTACK_POWER;
 
 	//モデルデータのロード
-	hModel_ = Model::Load("PlayerFbx/playerTest2.fbx");
+	hModel_ = Model::Load("PlayerFbx/playerTest.fbx");
 	assert(hModel_ >= 0);
 	
 }
